b2a49de20d36134eb63f7284fe375580ddd36212
sopanpatil <sopanpatil@protonmail.com>
2016-06-17 22:47:09 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='hydroutils',
+      version='1.0',
+      description='Utility classes for building and running hydrological models',
+      author='Sopan Patil and Marc Stieglitz',
+      license='MIT',
+      packages=['hydroutils'],
+      install_requires=['numpy', 'scipy'],
+      classifiers=[
+          'Programming Language :: Python :: 2.7',
+          'License :: OSI Approved :: MIT License',
+          'Topic :: Scientific/Engineering',
+          'Intended Audience :: Science/Research'
+      ],
+      )
